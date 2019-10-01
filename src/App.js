98686cf692645875991da810a5e540848fc26a6a
PequeCeci <hey.ceci.lets.go@gmail.com>
2019-10-01 10:38:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({ data: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+          <div className="container">
+              <form className="text">
+                <input className="text__input" type="text" 
+                placeholder="Ingresa un texto" onChange={this.handleChange} />
+                <button className="text__button">
+                    <i className="fas fa-pencil-alt"></i>
+                </button>
+            </form>
+            <p className="paragraph">{this.state.data}</p>
+          </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
